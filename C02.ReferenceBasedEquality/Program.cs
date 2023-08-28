769ee642c260778937159ded136a736d85198197
unknown <abdallahaliahmed517@gmail.com>
2023-08-28 15:03:24 +0300

    RRR

--- C02.ReferenceBasedEquality/Program.cs
+++ C02.ReferenceBasedEquality/Program.cs
@@ -10,4 +10,12 @@ var p2 = new Point(2, 3);
 Console.WriteLine($"p1: ({p1.X}, {p1.Y})"); // p1: (2, 3)
 Console.WriteLine($"p2: ({p2.X}, {p2.Y})"); // p2: (2, 3)
 
+Console.WriteLine($"p1.Equals(p2) = {p1.Equals(p2)}");    // -> print False;
+Console.WriteLine($"Object.ReferenceEquals(p1, p2) =" +
+                $"{Object.ReferenceEquals(p1, p2)}");     // -> print False
+
+p1 = p2;
+
 Console.WriteLine($"p1.Equals(p2) = {p1.Equals(p2)}");    // -> print True;
+Console.WriteLine($"Object.ReferenceEquals(p1, p2) =" +
+                $"{Object.ReferenceEquals(p1, p2)}");     // -> print True
