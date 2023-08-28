a358b01eb9493e8f6913609ef20ee33e045b4c21
unknown <abdallahaliahmed517@gmail.com>
2023-08-28 14:33:04 +0300

    RRR

---
+++ _01_ValueBasedEquality/Point.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+struct Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ _01_ValueBasedEquality/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Drawing;
+
+var p1 = new Point(2, 3);
+var p2 = new Point(2, 3);
+
+Console.WriteLine($"p1: ({p1.X}, {p1.Y})"); // p1: (2, 3)
+Console.WriteLine($"p2: ({p2.X}, {p2.Y})");
+
+Console.WriteLine($"p1.Equals(p2) = {p1.Equals(p2)}");
