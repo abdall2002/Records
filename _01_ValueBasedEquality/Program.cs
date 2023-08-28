fd3e751e7e3d04a4eb469fc7b8973ef3e0258b96
unknown <abdallahaliahmed517@gmail.com>
2023-08-28 14:49:02 +0300

    RRR

--- _01_ValueBasedEquality/Program.cs
+++ _01_ValueBasedEquality/Program.cs
@@ -8,6 +8,9 @@ var p1 = new Point(2, 3);
 var p2 = new Point(2, 3);
 
 Console.WriteLine($"p1: ({p1.X}, {p1.Y})"); // p1: (2, 3)
-Console.WriteLine($"p2: ({p2.X}, {p2.Y})");
+Console.WriteLine($"p2: ({p2.X}, {p2.Y})"); // p2: (2, 3)
 
-Console.WriteLine($"p1.Equals(p2) = {p1.Equals(p2)}");
+Console.WriteLine($"p1.Equals(p2) = {p1.Equals(p2)}");    // -> print True;
+
+
+
