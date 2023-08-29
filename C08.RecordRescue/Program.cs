8e31c97ffbb34e966e560c74080e63865d3cadf8
unknown <abdallahaliahmed517@gmail.com>
2023-08-29 14:45:36 +0300

    RRR

---
+++ C08.RecordRescue/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+var p1 = new Point(2, 3);
+var p2 = new Point(2, 3);
+
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+
+Console.WriteLine($"p1.Equals(p2): {p1.Equals(p2)}");     // -> print True;
+Console.WriteLine($"(p1 == p2): {p1 == p2}");             // -> print True;
+
+record Point
+{
+    // override object equal
+    // implement IEquatable <Point>
+    // override object GetHashCode
+    // override ==, !=
+    // override ToString();
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X;
+    public int Y;
+}
