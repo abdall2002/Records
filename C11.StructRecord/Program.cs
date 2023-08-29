e56a2927fad65ee73c134f374fb7e2c82f6dfd8a
unknown <abdallahaliahmed517@gmail.com>
2023-08-29 15:19:26 +0300

    RRR

---
+++ C11.StructRecord/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+var p1 = new Point(2, 3);
+// p1.X = 10; // position readonly struct record are immutable
+Console.WriteLine(p1);
+
+//public record struct Point(int X, int Y);
+public readonly record struct Point(int X, int Y);
