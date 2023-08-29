19f959d111dcf7519799132c76e960117a79316a
unknown <abdallahaliahmed517@gmail.com>
2023-08-29 15:23:34 +0300

    RRR

---
+++ C12.WithExpression/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Drawing;
+
+var p1 = new Point(2, 3);
+
+var p2 = new Point(4, p1.Y);
+
+var p3 = p1 with { X = 4 };
+
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.WriteLine(p3);
+
+
+record Point(int X, int Y);
