c4b55f71dea35068eccdb582d31ba887c96916a0
unknown <abdallahaliahmed517@gmail.com>
2023-08-29 14:14:55 +0300

    RRR

---
+++ C07.OverrideToString/Point.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C07.OverrideToString
+{
+    class Point : IEquatable<Point>
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+
+        public Point() { }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Point p = obj as Point;
+
+            return this.Equals(p);
+        }
+
+        public bool Equals(Point point)
+        {
+            if (point is null)
+                return false;
+
+            return point.X == X && point.Y == Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public static bool operator ==(Point lhs, Point rhs)
+        {
+            if (lhs is null)
+            {
+                if (rhs is null)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return lhs.Equals(rhs);
+        }
+        public static bool operator !=(Point lhs, Point rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override string ToString()
+        {
+            return $"{{ X = {X}, Y = {Y} }}";
+        }
+    }
+
+}
---
+++ C07.OverrideToString/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+using C07.OverrideToString;
+
+var p1 = new Point(2, 3);
+var p2 = new Point(2, 3);
+
+Console.WriteLine(p1);     // -> print { X = 2, Y = 3 }
+Console.WriteLine(p2);     // -> print { X = 2, Y = 3 }
